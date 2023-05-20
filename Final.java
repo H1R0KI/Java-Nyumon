60798ce46080ca15a86eaaa22c0ab4e2349dde07
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-05-20 13:51:32 +0000

    chapter1-3

---
+++ Final.java
@@ -0,0 +1,8 @@
+public class Final {
+  public static void main(String args[]) {
+    final double PI = 3.14; //型の前に final を入れることにより初期値を固定できる
+    int pie = 5;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * PI);
+  }
+}
