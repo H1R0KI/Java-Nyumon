4043bb9f166c518debe65c8242e86d25d2c65e2c
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-05-24 15:44:20 +0000

    chapter2-7

---
+++ chapter2/c212.java
@@ -0,0 +1,8 @@
+public class c212 {
+  public static void main(String args[]) {
+    int a = 5;
+    int b = 3;
+    int m = Math.max(a,b); //双方を比較し、大きい方を表示
+    System.out.println("比較：" + a + "と" + b + "で大きい方は" + m);
+  }
+}
---
+++ chapter2/c214.java
@@ -0,0 +1,6 @@
+public class c214 {
+  public static void main(String args[]) {
+    int r = new java.util.Random().nextInt(100);
+    System.out.println("お前は" + r + "歳だろ");
+  }
+}
