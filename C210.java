4312c60aa0e80a6dd9b7190cc9f6ea9035769fd2
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-05-24 15:27:59 +0000

    chapter2-6

---
+++ C210.java
@@ -0,0 +1,8 @@
+public class C210 {
+  public static void main(String args[]) {
+    String name = "すがわら";
+    String message;
+    message = name + "さん、こんにちは";
+    System.out.println(message);
+  }
+}
