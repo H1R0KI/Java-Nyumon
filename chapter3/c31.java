a20099992b0a5f428883ba52716e40caf252b8bb
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-05-25 16:03:06 +0000

    chapter3-3

---
+++ chapter3/c31.java
@@ -0,0 +1,10 @@
+public class c31 {
+  public static void main(String args[]) {
+    boolean tenki = true;
+    if (tenki == true) {
+      System.out.println("洗濯します");
+    } else {
+      System.out.println("DVDをみます");
+    }
+  }
+}
