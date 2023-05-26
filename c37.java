f0f0013cc5bf694616ffb683896b07dfc42ba0df
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-05-26 13:38:53 +0000

    chapter3-5

---
+++ c37.java
@@ -0,0 +1,10 @@
+public class c37 {
+  public static void main(String args[]) {
+    for (int i = 0; i < 10; i++) {
+    // i = 0 は初期化処理　最初に1回だけ実行
+    // i < 10は繰り返し条件
+    // i++は繰り返し事処理　ループ時に変数の値を１ずつ増やす
+      System.out.println("こんにちは");
+    }
+  }
+}
---
+++ c38.java
@@ -0,0 +1,7 @@
+public class c38 {
+  public static void main(String args[]) {
+    for (int i = 0; i < 5; i++) {
+      System.out.print("現在" + (i + 1) + "周目→");
+    }
+  }
+}
