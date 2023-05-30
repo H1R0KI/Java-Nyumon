a17b43226075686c2c16cf5114fe7b174b122dd3
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-05-30 16:20:32 +0000

    chapter4-18

---
+++ chapter4/c414.java
@@ -0,0 +1,8 @@
+public class c414 {
+  public static void main(String args[]) {
+    int[] scores = {20, 30, 40, 50, 80};
+    for (int value : scores) { //拡張for文 valueを使用
+      System.out.println(value);
+    }
+  }
+}
---
+++ chapter4/c418.java
@@ -0,0 +1,12 @@
+public class c418 {
+  public static void main(String args[]) {
+    int[] [] scores = new int[2] [3]; //多次元配列
+    scores[0] [0] = 40;
+    scores[0] [1] = 50;
+    scores[0] [2] = 60;
+    scores[1] [0] = 80;
+    scores[1] [1] = 60;
+    scores[1] [2] = 70;
+    System.out.println(scores[1] [1]);
+  }
+}
