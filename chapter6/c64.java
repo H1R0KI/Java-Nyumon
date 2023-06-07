4cb0d983448bc788388ad4d68cca84e90bf8d2f0
H1R0KI <122168203+H1R0KI@users.noreply.github.com>
2023-06-07 13:29:59 +0000

    chapter6-1

---
+++ chapter6/c64.java
@@ -0,0 +1,8 @@
+public class c64 {
+  public static void main(String[] args) {
+    int a = 10; int b = 2;
+    int total = c62.tasu(a,b);
+    int delta = c62.hiku(a,b);
+    System.out.println("足すと" + total + "、引くと" + delta);
+  }
+}
